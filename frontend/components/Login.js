c74218e30b8750a03c4134e8e65b448bb004c528
Kahina <bndkahi73@gmail.com>
2025-02-13 12:32:36 +0100

    updated Home/ Login

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -1,9 +1,23 @@
-import styles from '../styles/Login.module.css';
+import styles from "../styles/Login.module.css";
+import Signin from "./SignIn";
+import Signup from "./SignUp";
+
 
 function Login() {
   return (
     <div>
-      
+      <main className={styles.main}>
+      <div>
+     <img className={styles.homeImg} src="/twitter_bg.png" alt="imgHome"></img>
+      </div>
+      <div className={styles.homeRightPart}>
+        <img className={styles.logo} src="logo_twitter.png" alt="logo"></img>
+        <h1 className={styles.titre}>See what's <br></br>happening</h1>
+        <h4 className={styles.titre2}>Join Hackatweet today.</h4>
+        
+      </div>
+
+      </main>
     </div>
   );
 }
