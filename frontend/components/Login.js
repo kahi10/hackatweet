06d1398d4dbd0098556d8c1ff77ae633da00973d
yirui Dupuy <yirui.hao@gmail.com>
2025-02-13 14:39:11 +0100

    frontend page login

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -4,19 +4,26 @@ import Signup from "./SignUp";
 
 
 function Login() {
+
+
+
   return (
     <div>
       <main className={styles.main}>
-      <div>
-     <img className={styles.homeImg} src="/twitter_bg.png" alt="imgHome"></img>
-      </div>
-      <div className={styles.homeRightPart}>
-        <img className={styles.logo} src="logo_twitter.png" alt="logo"></img>
-        <h1 className={styles.titre}>See what's <br></br>happening</h1>
-        <h4 className={styles.titre2}>Join Hackatweet today.</h4>
-        
-      </div>
+        <div className={styles.containerIMG}>
+          <img className={styles.logosuperpose} src="logo_twitter.png"></img>
+        </div>
+        <div className={styles.homeRightPart}>
+          <img className={styles.logo} src="logo_twitter.png" alt="logo"></img>
 
+          <h1 className={styles.titre}>
+            See what's <br></br>happening
+          </h1>
+          <h4 className={styles.titre2}>Join Hackatweet today.</h4>
+          <Signin></Signin>
+          <p className={styles.loginP}>Already have an account?</p>
+          <Signup></Signup>
+        </div>
       </main>
     </div>
   );
