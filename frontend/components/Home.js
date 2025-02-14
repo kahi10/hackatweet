7487a25534a9170478efd7e3e0d057dca0551020
yirui Dupuy <yirui.hao@gmail.com>
2025-02-14 12:02:59 +0100

    frontend composant trends

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,13 +1,14 @@
 import styles from "../styles/Home.module.css";
 import Login from "./Login";
+import Trends from "./Trends";
 
 
 function Home() {
   return (
     <div>
       <h1>Hello word</h1>
+      <Trends></Trends>
     </div>
-
   );
 }
 
---
+++ frontend/components/Trends.js
@@ -0,0 +1,19 @@
+import styles from "../styles/Trends.module.css";
+import { useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+
+
+function Trends() {
+
+
+
+
+    return (
+      <div className={styles.trendsContainer}>
+        <h1>Trends</h1>
+      </div>
+  
+    );
+  }
+  
+  export default Trends;
