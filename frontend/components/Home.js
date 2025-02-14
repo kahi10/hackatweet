8a292f5d315e759273169d28152d06934aabcae9
Kahina <bndkahi73@gmail.com>
2025-02-14 11:55:07 +0100

    added Home

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,13 +1,34 @@
 import styles from "../styles/Home.module.css";
-import Login from "./Login";
-
+import { useState, useEffect } from "react";
+import { useSelector } from 'react-redux';
 
 function Home() {
+  const [firstName, setFirstName] = useState('');
+  const [username, setUsername] = useState('');
+  const user = useSelector((state) => state.users.value);
+ useEffect(() => {
+  setFirstName(user.firstname);
+  setUsername(user.username);
+ }, [user])
   return (
     <div>
-      <h1>Hello word</h1>
+      <div className={styles.userSection}>
+        <div className={styles.logo}>
+          <img className={styles.logoTweet} src="logo_twitter.png" alt="logoTweet" />
+        </div>
+        <div>
+          <img className={styles.avatar} src="avatar.png" alt="avatar" />
+          <div>
+            <p>{firstName}</p>
+            <p>@{username}</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.homeSection}>
+        <h1>Home</h1>
+      </div>
+      <div className={styles.trendsSection}></div>
     </div>
-
   );
 }
 
