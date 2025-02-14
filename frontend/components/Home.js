6d1590ea5dde1533bcdd990791585a9a831fa04e
Mathilde <mathilde.roussille@hotmail.fr>
2025-02-14 12:00:58 +0100

    Tweet

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -5,12 +5,7 @@ import Login from "./Login";
 function Home() {
   return (
     <div>
-<<<<<<< HEAD
-
-      <Login></Login>
-=======
       <h1>Hello word</h1>
->>>>>>> c5873f6bcdc199e4635b1f07393733cc45ffcd07
     </div>
 
   );
