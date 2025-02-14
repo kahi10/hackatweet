29ff13a29c891ac09c8fb703bdf50ccd92deaeff
yirui Dupuy <yirui.hao@gmail.com>
2025-02-14 11:34:04 +0100

    backend route tweets

--- backend/models/tweets.js
+++ backend/models/tweets.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const tweetSchema = mongoose.Schema({
     contenu: String,
     date: Date,
-    createperson: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
-    likeperson: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],
+    createPerson: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+    likePerson: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],
 
 });
 
